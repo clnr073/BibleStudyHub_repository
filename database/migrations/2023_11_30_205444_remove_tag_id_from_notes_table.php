2ae27121ec22be3e85246f46b37c0a8c6a859c49
clnr073 <nengfan073@gmail.com>
2023-11-30 22:05:29 +0900

    fix: #13 Delete unnecessary columns

---
+++ database/migrations/2023_11_30_205444_remove_tag_id_from_notes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('notes', function (Blueprint $table) {
+            // dropForeignメソッドで外部キーを除外
+            // (外部キー名は{テーブル名}_{カラム名}_foreign)
+            $table->dropForeign('notes_tag_id_foreign');
+            $table->dropColumn('tag_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('notes', function (Blueprint $table) {
+            $table->foreignId('tag_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+};

3001933e7f757b849e29fb2834f358bdd976907c
clnr073 <nengfan073@gmail.com>
2023-12-18 18:37:33 +0900

    feat: #32 environment building

--- config/app.php
+++ config/app.php
@@ -181,7 +181,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        
         /*
          * Package Service Providers...
          */
@@ -194,6 +194,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
+        CloudinaryLabs\CloudinaryLaravel\CloudinaryServiceProvider::class,
+
 
     ],
 

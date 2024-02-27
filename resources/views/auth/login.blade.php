839f0a2f39d028028f84ee98d79f30c9e54d73ab
clnr073 <nengfan073@gmail.com>
2024-02-27 16:34:16 +0900

    fix: #7 Undo string

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,7 +40,7 @@
             @endif
 
             <x-primary-button class="ml-3">
-                {{ __('ログイン') }}
+                {{ __('Log in') }}
             </x-primary-button>
         </div>
     </form>

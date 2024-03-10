11bbc4593d38afae173b88bd8e41a74c7a0426b9
clnr073 <nengfan073@gmail.com>
2024-03-10 17:14:15 +0900

    Fix comparison in select option values

--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -7,8 +7,8 @@
                         @csrf
                         <div class="flex items-center justify-between">
                             <select class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm mt-1 block" name="note[public]">
-                                <option value="1" {{ $public_value == true ? 'selected' : '' }}>公開ノート</option>
-                                <option value="0" {{ $public_value == false ? 'selected' : '' }}>非公開ノート</option>
+                                <option value="1" {{ $public_value == 'true' ? 'selected' : '' }}>公開ノート</option>
+                                <option value="0" {{ $public_value == 'false' ? 'selected' : '' }}>非公開ノート</option>
                             </select>
                             <div>
                                 <div class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
